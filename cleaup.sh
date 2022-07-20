a06ba10cc79b938353397c669cc28e81fffdb9b9
dmarcus-wire <dmarcus@redhat.com>
2022-07-20 21:13:17 +0000

    in-work batch inferencing and serving

--- cleaup.sh
+++ cleaup.sh
@@ -2,10 +2,10 @@
 # usage from the terminal in the same location as the cleanup.sh run './cleanup.sh'
 
 printf "${GREEN}Delete models\n\n"
-rm -rf models/sepsis_pipeline
+rm -rf models/sepsis
 
 printf "${GREEN}Delete pipeline artifacts\n\n"
-rm -rf pipeline/train_pipeline
+rm -rf pipeline/
 
 printf "${GREEN}Delete ML source code\n\n"
 rm src/{pipeline.py,training_code.py}
