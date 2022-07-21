3f6159e52172fe878636f2a44780a31424332b21
dmarcus-wire <dmarcus@redhat.com>
2022-07-21 00:28:55 +0000

    basic and advanced pipeline working

--- .ipynb_checkpoints/cleaup-checkpoint.sh
+++ .ipynb_checkpoints/cleaup-checkpoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # usage from the terminal in the same location as the cleanup.sh run './cleanup.sh'
+# recommended to run between notebook cell runs as artficat id mismatch will cause errors
 
 printf "${GREEN}Delete models\n\n"
 rm -rf models/sepsis
 
 printf "${GREEN}Delete pipeline artifacts\n\n"
-rm -rf pipeline/
+rm -rf pipeline/*
 
 printf "${GREEN}Delete ML source code\n\n"
-rm src/{pipeline.py,training_code.py}
+rm src/*
+
+printf "${GREEN}Delete .ipynb_checkpoints in data folders\n\n"
+rm -rf data/{training,serving}_data/.ipynb_checkpoints
